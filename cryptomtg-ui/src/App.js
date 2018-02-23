66bf536b9035239f9b3a5ee4c152804392038a74
Oliver Norton <onorton125@gmail.com>
2018-02-23 22:28:28 +0000

    Added tab menu with tabs. Need to fill content.

--- cryptomtg-ui/src/App.js
+++ cryptomtg-ui/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Crypto Magic the Gathering</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Tabs>
+          <TabList>
+            <Tab>My Cards</Tab>
+            <Tab>Cards on Auction</Tab>
+            <Tab>Trades</Tab>
+          </TabList>
+
+          <TabPanel>
+            <h2>Any content 1</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 3</h2>
+          </TabPanel>
+      </Tabs>
       </div>
     );
   }
